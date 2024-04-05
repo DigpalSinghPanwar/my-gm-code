e386e942c59781b59517724b79b0e70c5f778cd6
DigpalSinghPanwar784 <singh.digpal784@gmail.com>
2024-04-05 21:52:59 +0530

    added cors.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 const memberRouter = require("./routes/memberRoutes");
 const userRouter = require("./routes/userRoutes");
 
 // global middleware
+
+app.use(cors());
+
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
